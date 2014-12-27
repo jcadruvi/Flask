262b1c351dd1b71ad2a3925aed9e64fd217cc5b0
Josh Cadruvi <jcadruvi@imatchative.com>
2014-12-27 14:03:18 -0800

    Removing the News folder.

--- website/News/NewsService.py
+++
@@ -1,5 +0,0 @@
-from .NewsRepository import NewsRepository
-
-class NewsService(object):
-    def GetNews(self):
-        return NewsRepository().GetNews()
--- website/News/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from .NewsService import NewsService
-
-bp = Blueprint('news', __name__)
-newsBluePrint = Blueprint('news', __name__)
-
-@newsBluePrint.route('/api/News', methods=['GET'])
-def News():
-    return jsonify(NewsService().GetNews())
